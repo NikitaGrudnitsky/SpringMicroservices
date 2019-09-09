22a31fd7d24195e360dfc802b474d3f3ab581e6b
Nick Grudnitsky <grud97@gmail.com>
2019-09-09 13:50:33 +0300

    Hystrix

--- ingredient-client/src/main/java/tacos/ingredientclient/resttemplate/IngredientServiceClient.java
+++ ingredient-client/src/main/java/tacos/ingredientclient/resttemplate/IngredientServiceClient.java
@@ -1,11 +1,18 @@
 package tacos.ingredientclient.resttemplate;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Conditional;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import tacos.ingredientclient.Ingredient;
 
@@ -18,17 +25,57 @@ public class IngredientServiceClient {
   public IngredientServiceClient(@LoadBalanced RestTemplate rest) {
     this.rest = rest;
   }
-  
+
+  @HystrixCommand(fallbackMethod="getDefaultIngredientDetails")
   public Ingredient getIngredientById(String ingredientId) {
     return rest.getForObject(
-        "http://ingredient-service/ingredients/{id}", 
-        Ingredient.class, ingredientId);
+            "http://ingredient-service/ingredients/{id}",
+            Ingredient.class, ingredientId);
   }
-  
+
+  private Ingredient getDefaultIngredientDetails(String ingredientId) {
+    if (ingredientId.equals("FLTO")) {
+      return new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP);
+    } else if (ingredientId.equals("GRBF")) {
+      return new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN);
+    } else {
+      return new Ingredient("CHED", "Shredded Cheddar", Ingredient.Type.CHEESE);
+    }
+  }
+
+
+  @HystrixCommand(fallbackMethod="getDefaultIngredients",
+          commandProperties={
+                  @HystrixProperty(
+                          name="circuitBreaker.requestVolumeThreshold",
+                          value="30"),
+                  @HystrixProperty(
+                          name="circuitBreaker.errorThresholdPercentage",
+                          value="25"),
+                  @HystrixProperty(
+                          name="metrics.rollingStats.timeInMilliseconds",
+                          value="20000"),
+                  @HystrixProperty(
+                          name="circuitBreaker.sleepWindowInMilliseconds",
+                          value="60000")
+          })
   public Iterable<Ingredient> getAllIngredients() {
-    Ingredient[] ingredients = rest.getForObject(
-        "http://ingredient-service/ingredients", Ingredient[].class);
-    return Arrays.asList(ingredients);
+    ParameterizedTypeReference<List<Ingredient>> stringList =
+            new ParameterizedTypeReference<List<Ingredient>>() {};
+    return rest.exchange(
+            "http://ingredient-service/ingredients", HttpMethod.GET,
+            HttpEntity.EMPTY, stringList).getBody();
+  }
+
+  private Iterable<Ingredient> getDefaultIngredients() {
+    List<Ingredient> ingredients = new ArrayList<>();
+    ingredients.add(new Ingredient(
+            "FLTO", "Flour Tortilla", Ingredient.Type.WRAP));
+    ingredients.add(new Ingredient(
+            "GRBF", "Ground Beef", Ingredient.Type.PROTEIN));
+    ingredients.add(new Ingredient(
+            "CHED", "Shredded Cheddar", Ingredient.Type.CHEESE));
+    return ingredients;
   }
   
 }
