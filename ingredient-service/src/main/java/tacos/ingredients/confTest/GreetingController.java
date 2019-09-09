e7e960f4055c49e7ec9406f11b91a28f65b468c4
Nick Grudnitsky <grud97@gmail.com>
2019-09-09 12:29:39 +0300

    Refreshing configuration properties on the fly

---
+++ ingredient-service/src/main/java/tacos/ingredients/confTest/GreetingController.java
@@ -0,0 +1,16 @@
+package tacos.ingredients.confTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+    private final GreetingProps props;
+    public GreetingController(GreetingProps props) {
+        this.props = props;
+    }
+    @GetMapping("/hello")
+    public String message() {
+        return props.getMessage();
+    }
+}
